f03684cc46c037fa9ccec7a35ef5ec4be33989bd
Dana Kelly <muvceski@gmail.com>
2015-05-19 20:46:31 -0500

    JS addition

---
+++ script.js
@@ -0,0 +1,14 @@
+var userNumber = prompt("Pick a number, any number...");
+var computerNumber = Math.floor((Math.random() *50) + 1);
+if (userNumber < computerNumber){
+alert("your number was too low") 
+} else {alert("your number is too high")
+};
+ 	
+
+
+
+
+
+
+
